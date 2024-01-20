f6e84a688b92c3fcfbd2ebbd1b0da19d088cf229
AliDavid <AliDavoodi98@github.com>
2024-01-20 04:47:55 +0000

    Update Kafka source in TrendDetectionJob

--- flink-app/src/main/java/flinkJob/trendDetection/TrendDetectionJob.java
+++ flink-app/src/main/java/flinkJob/trendDetection/TrendDetectionJob.java
@@ -4,13 +4,14 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-//import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 //import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 
 import java.util.Properties;
@@ -30,22 +31,33 @@ public class TrendDetectionJob {
         props.setProperty("group.id", "flink-reddit-consumer");
 
         // Set up the Kafka source
-        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(
-                "worldnews",
-                new SimpleStringSchema(),
-                props);
+        // FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(
+        // "worldnews",
+        // new SimpleStringSchema(),
+        // props);
 
-        // KafkaSource<String> consumer = KafkaSource.<String>builder()
-        // .setBootstrapServers("localhost:9092")
-        // .setTopics("worldnews")
-        // .setGroupId("flink-reddit-consumer")
-        // .setValueOnlyDeserializer(new SimpleStringSchema())
-        // .build();
+        KafkaSource<String> consumer = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("worldnews")
+                .setGroupId("flink-reddit-consumer")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
 
         // Define the data processing pipeline
         // DataStream<String> stream = env.addSource(consumer);
 
-        DataStream<String> stream = env.addSource(consumer)
+        // DataStream<String> stream = env.addSource(consumer)
+        // .map(new MapFunction<String, String>() {
+        // @Override
+        // public String map(String value) throws Exception {
+        // System.out.println("Received from Kafka: " + value);
+        // return value;
+        // }
+        // });
+
+        DataStream<String> stream = env.fromSource(consumer,
+                WatermarkStrategy.noWatermarks(), "Kafka Source")
                 .map(new MapFunction<String, String>() {
                     @Override
                     public String map(String value) throws Exception {
@@ -53,9 +65,6 @@ public class TrendDetectionJob {
                         return value;
                     }
                 });
-
-        // DataStream<String> stream = env.fromSource(consumer,
-        // WatermarkStrategy.noWatermarks(), "Kafka Source");
 
         DataStream<Trend> trends = stream
                 .map(json -> JsonParser.parseJsonPost(json)) // Parse JSON to Post objects
